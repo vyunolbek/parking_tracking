0e585b80d9162d3e1d43050b9dda2904c2816926
Данила Вьюнов <cliffordik14@gmail.com>
2022-12-04 21:43:46 +0300

    Switching to YOLOv5

--- main.py
+++ main.py
@@ -6,21 +6,23 @@ import cv2 as cv
 import numpy as np
 import argparse
 import xml.etree.ElementTree as ET
+import torch
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--source', dest='destination_source', default='./img/rrr.jpg')
-parser.add_argument('--labeled', dest='destination_labeled', default='./labeled/rrr.xml')
+parser.add_argument('--source', dest='path_source', default='./img/rrr.jpg')
+parser.add_argument('--labeled', dest='path_labeled', default='./labeled/rrr.xml')
 args = parser.parse_args()
 
-img = cv.imread(args.destination_source)
-imgg = to_pil_image(img)
+img = cv.imread(args.path_source)
+img = cv.resize(img, (img.shape[1] // 2, img.shape[0] // 2))
 
+tree = ET.parse(args.path_labeled)
 
-tree = ET.parse(args.destination_labeled)
-
-weights = FasterRCNN_ResNet50_FPN_V2_Weights.DEFAULT
-model = fasterrcnn_resnet50_fpn_v2(weights=weights, box_score_thresh=0.4)
-model.eval()
+model = torch.hub.load('ultralytics/yolov5', 'yolov5m', pretrained=True)
+model.conf = 0.1
+model.multi_label = True
+model.iou = 0.3
+model.classes = [2, 7, 5]
 
 root = tree.getroot()
 a = []
@@ -28,23 +30,21 @@ b = []
 for elem in root:
     for subelem in elem:
             for sub in subelem:
-                b.append(int(sub.text))
+                b.append(int(sub.text) // 2)
 
 for i in range(0, len(b), 4):
     a.append(b[i:i+4])
 
-preprocess = weights.transforms()
 
+prediction = model(img)
+print([[float(prediction.xyxy[0][i][4]), model.names[int(prediction.xyxy[0][i][-1])], int(prediction.xyxy[0][i][-1])] for i in range(len(prediction.xyxy[0]))])
 
-batch = [preprocess(imgg)]
-
-prediction = model(batch)[0]
 cnt = 0
 used = []
 for place in a:
     flag = True
-    for rect in range(len(prediction['boxes'])):
-        box = list(map(int, prediction['boxes'][rect]))
+    for rect in range(len(prediction.xyxy[0])):
+        box = list(map(int, prediction.xyxy[0][rect]))
         dot = [(box[0] + (box[2] - box[0]) // 2),  (box[1] - (box[1] - box[3]) // 2)]
         if (dot[0] >= place[0] and dot[1] >= place[1] and dot[0] <= place[2] and dot[1] <= place[3]):
             cv.circle(img, (dot[0],  dot[1]), radius=5, thickness=4, color=(0, 200, 0))
